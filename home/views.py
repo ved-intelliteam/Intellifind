cd51a5c3cab5763d10344410f4c4f132a3573c2a
ved-intelliteam <ved@intelliteam.ai>
2024-11-19 16:13:57 +0530

    added fixed alignmens and updated elements

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Subscriber
+from .forms import SubscriberForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            message = "Thanks for subscribing!"
+            return render(request, 'home/homepage.html', {'message': message})
     return render(request, "home/homepage.html")
